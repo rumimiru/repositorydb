1cfee5fad670bc72d2df9ec4e34b1087a745f126
Francine Nicole <chibierror@gmail.com>
2026-04-18 21:26:01 +0800

    Update demo.js

--- node-demo/demo.js
+++ node-demo/demo.js
@@ -1,10 +1,7 @@
-// Import File System module
 const fs = require('fs');
 
-// Define content
 const content = "Hello! This file was created using Node.js.";
 
-// Write to file
 fs.writeFile('message.txt', content, (err) => {
   if (err) {
     console.error("Error writing file:", err);
